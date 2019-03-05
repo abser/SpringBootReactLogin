0d61d8e13de1308384a2258c7585a0ae1903a751
Nurul Abser <nurul.abser@income.com.sg>
2019-03-05 17:09:18 +0800

    first commit

---
+++ src/main/java/com/react/springboot/springbootreactlogin/SpringBootReactLogin.java
@@ -0,0 +1,13 @@
+package com.react.springboot.springbootreactlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactLogin {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactLogin.class, args);
+	}
+
+}
